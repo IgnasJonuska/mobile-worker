78965706f6b8e53f59c295abdbb5db87536f01a3
Ignas <jonuska.ignas@gmail.com>
2020-09-09 21:42:35 +0300

    week display fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddTaskComponent } from './fake-pages/add-task/add-task.component';
 import { TimesheetComponent } from './widgets/timesheet/timesheet.component';
+import { DummyTimesheetComponent } from './fake-pages/dummy-timesheet/dummy-timesheet.component';
 
 const routes: Routes = [
   { path: '', component:TimesheetComponent },
   { path: 'add-task', component: AddTaskComponent },
+  { path: 'dummy-timesheet', component: DummyTimesheetComponent}
 ];
 
 @NgModule({
---
+++ src/app/fake-pages/dummy-timesheet/dummy-timesheet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy-timesheet',
+  templateUrl: './dummy-timesheet.component.html',
+  styleUrls: ['./dummy-timesheet.component.css']
+})
+export class DummyTimesheetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
